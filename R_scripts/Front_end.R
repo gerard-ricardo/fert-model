d14f02f3a7b9135051afd2b963660722d4360b2a
gerard-ricardo <g.ricardo@aims.gov.au>
2024-10-11 14:04:54 +1000

    n

--- R_scripts/Front_end.R
+++ R_scripts/Front_end.R
@@ -14,6 +14,11 @@ source("./R_scripts/settings.R")           # Model settings
 source("./R_scripts/1_print_outputs.R") # Model outputs
 source("./R_scripts/Backend.R") # Model backend logic
 
+# Creating a task-specific folder inside 'out' directory to avoid overwriting results
+dir_name <- "./out/test1"  #NAME THE FOLDER
+dir.create(file.path(getwd(),  dir_name), showWarnings = T)
+new_folder_path <- file.path(getwd(), dir_name)
+
 
 
 # ============================================================================
@@ -32,7 +37,7 @@ source("./R_scripts/Backend.R") # Model backend logic
 # ============================================================================
 (df = data.frame(
     colony_diam = c(20),
-    patch_type = c('patch_bot'),#
+    patch_type = c('patch_bot'),
     depth_m = c(3),
     flow_rate = c(0.2)))
 
@@ -45,7 +50,7 @@ source("./R_scripts/Backend.R") # Model backend logic
 for(i in 1:nrow(df)) {
   input1 <- paste(as.character(unname(df[i,])), collapse="_")
   input_df <- df[i,]
-  run_model(scenario = "test", debug = TRUE, store = TRUE, execution_mode = 'parallel', input1 = input1, input_df = input_df,
+  run_model(scenario = "test", store = T, input1 = input1, input_df = input_df,
             colony_diam = df[i, 'colony_diam'], den_cell = df[i, 'den_cell'], int_col_spac = df[i, 'int_col_spac'], patch_type = df[i, 'patch_type'],
             depth_m = df[i, 'depth_m'], flow_rate = df[i, 'flow_rate'], no_height = df[i, 'no_height'], no_width = df[i, 'no_height'],
             tb = df[i, 'tb'], fe = df[i, 'fe'], E0 = df[i, 'E0'], patch_density  = df[i, 'patch_density'], spe_speed  = df[i, 'spe_speed'],
