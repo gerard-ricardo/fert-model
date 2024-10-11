e2bbfeeee6ec95b4d2868ac6d947156b53ef94ba
gerard-ricardo <g.ricardo@aims.gov.au>
2024-10-11 15:32:27 +1000

    v

--- scratch/update_workflow.R
+++ scratch/update_workflow.R
@@ -1,6 +1,3 @@
 
-
-# Copy 1_teontodd_backend.R to '1 Github coral fert model.R'
-# Copy to '1 Github coral fert model.R'
-# Copy to blank_commented.R and run cleaning functions
-# Push to Github (dont pull to avoid loosing scratch)
+# Copy to blank_commented.R and run cleaning functions individually
+# Push to Github (don't pull to avoid loosing scratch)
